d43ce0fb7dd18a66c6dc9a153e3037a021e4f96d
Jose Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2022-03-16 15:24:12 -0300

    Update the way we define the available dates

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -68,6 +68,15 @@ class _MyHomePageState extends State<MyHomePage> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  void listener(){
+    debugPrint('Changed');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -109,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 start: const TimeOfDay(hour: 12, minute: 0),
                 end: const TimeOfDay(hour: 13, minute: 15)),
             IntervalRange(
-                start: const TimeOfDay(hour: 18, minute: 0),
+                start: const TimeOfDay(hour: 18, minute: 30),
                 end: const TimeOfDay(hour: 22, minute: 30))
           ],
         ));
